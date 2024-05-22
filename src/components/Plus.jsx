1a635c04df9f145c21427247c0836023c7919815
Arturo Escobar <s.arturoec@gmail.com>
2024-05-22 16:13:43 -0600

    agregar componente Plus

---
+++ src/components/Plus.jsx
@@ -0,0 +1,17 @@
+export default function Plus() {
+  return (
+    <svg
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="size-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 4.5v15m7.5-7.5h-15"
+      />
+    </svg>
+  );
+}
