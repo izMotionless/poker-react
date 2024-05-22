85d3a428d36a05401831d9dd74d94787a6f7c65d
Arturo Escobar <s.arturoec@gmail.com>
2024-05-22 15:51:25 -0600

    agregar funcion al boton mezclar cartas para que al presionar haga un llamado a la API y nos regrese el UUID de la jugada

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import "./App.css";
 import ArrowPath from "./components/ArrowPath";
+
 function App() {
+  const [jugada, setJugada] = useState("");
+  const [disabledMezclarCartas, setDisabledMezclarCartas] = useState(false);
+
+  const handleMezclarCartas = async () => {
+    const response = await fetch("http://localhost:8080/v1/poker");
+    const UUID = await response.text();
+    setJugada(UUID);
+    setDisabledMezclarCartas(true);
+  };
+
   return (
     <>
       <h1>♦ Poker Game ♦</h1>
       <div className="card">
-        <button onClick={() => console.log("You clicked me!")}>
-          Mezclar Cartas
+        <button onClick={handleMezclarCartas} disabled={disabledMezclarCartas}>
+          <span> Mezclar Cartas </span>
           <ArrowPath />
         </button>
+        {jugada ? <p>Jugada : {jugada}</p> : null}
       </div>
     </>
   );
