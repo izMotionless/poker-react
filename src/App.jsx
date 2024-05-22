2dc0464c698d0fc2e7733e272d71bd925527b2eb
Arturo Escobar <s.arturoec@gmail.com>
2024-05-22 14:46:38 -0600

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>♦ Poker Game ♦</h1>
+      <div className="card">
+        <button onClick={() => console.log("You clicked me!")}>
+          Mezclar Cartas
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
